40ad4be929620ab21790818aa88d9594ef62cb17
Mark Polak <mark@thedutchies.com>
2014-12-14 21:40:46 +0100

    Finally name change

---
+++ src/model/ModelBase.js
@@ -0,0 +1,18 @@
+(function (d2) {
+    'use strict';
+
+    d2.ModelBase = ModelBase;
+
+    function ModelBase() {
+        this.create = create;
+        this.save = save;
+    }
+
+    function create() {
+
+    }
+
+    function save() {
+
+    }
+})(window.d2 = window.d2 || {});
