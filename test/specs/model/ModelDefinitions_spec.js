a1a0abaab8726fe73a8e8f70b6c3ab24d8339d05
Mark Polak <mark@thedutchies.com>
2015-01-19 12:04:44 +0100

    Re enable ModelDefinitions tests

--- test/specs/model/ModelDefinitions_spec.js
+++ test/specs/model/ModelDefinitions_spec.js
@@ -1,35 +1,34 @@
-//describe('D2 models', function () {
-//    'use strict';
-//    var models;
-//    var ModelDefinitions = d2.ModelDefinitions;
-//
-//    beforeEach(function () {
-//        //TODO: Figure out a way how to get a clean object without extending?
-//        models = new ModelDefinitions(); //jshint nonew:false
-//    });
-//
-//    it('should be an object', function () {
-//        expect(models).toEqual(jasmine.any(Object));
-//    });
-//
-//    describe('add method', function () {
-//        it('should be a function', function () {
-//            expect(models.add).toEqual(jasmine.any(Function));
-//        });
-//
-//        it('should add a property to the models object', function () {
-//            models.add('dataElement');
-//
-//            expect(models.dataElement).toBeDefined();
-//        });
-//
-//        it('should throw an error when trying to add something that already exists', function () {
-//            function shouldThrow() {
-//                models.add('dataElement');
-//            }
-//            models.add('dataElement');
-//
-//            expect(shouldThrow).toThrowError('Model dataElement already exists');
-//        });
-//    });
-//});
+describe('D2 models', function () {
+    'use strict';
+    var models;
+    var ModelDefinitions = require('d2/model/ModelDefinitions');
+
+    beforeEach(function () {
+        models = new ModelDefinitions(); //jshint nonew:false
+    });
+
+    it('should be an object', function () {
+        expect(models).toEqual(jasmine.any(Object));
+    });
+
+    describe('add method', function () {
+        it('should be a function', function () {
+            expect(models.add).toEqual(jasmine.any(Function));
+        });
+
+        it('should add a property to the models object', function () {
+            models.add('dataElement');
+
+            expect(models.dataElement).toBeDefined();
+        });
+
+        it('should throw an error when trying to add something that already exists', function () {
+            function shouldThrow() {
+                models.add('dataElement');
+            }
+            models.add('dataElement');
+
+            expect(shouldThrow).toThrowError('Model dataElement already exists');
+        });
+    });
+});
