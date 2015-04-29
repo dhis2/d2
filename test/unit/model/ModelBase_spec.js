dhis2/d2

commit d16392b0a61226454058869627ad2fae630b583e

model: Changes modelBase.validate to be async
Added a method to ModelValidation that will be used for querying the api/schemas endpoint.
This will be an async operation and therefore the validate() method should return a promise.
ModelBase.validate() might now return a false positive if as there are not yet messages returned
by the ModelValidation.validate method.

References #9
References #11

src/model/ModelBase.js
@@ -10,7 +10,8 @@ class ModelBase {
     /**
      * @method save
      *
-     * @returns {Promise} Returns a promise that resolves when the model has been saved or rejects with an error message.
+     * @returns {Promise} Returns a promise that resolves when the model has been saved
+     * or rejects with the result from the `validate()` call.
      *
      * @description
      * Checks if the model is dirty. When the model is dirty it will check if the values of the model are valid by calling
@@ -26,47 +27,79 @@ class ModelBase {
             return Promise.reject('No changes to be saved');
         }
 
-        if (this.validate().status) {
-            return this.modelDefinition.save(this)
-                .then(() => {
-                    //TODO: check the return status of the save to see if it was actually successful and not ignored
-                    this.dirty = false;
-                });
-        }
+        return this.validate()
+            .then(validationState => {
+                if (!validationState.status) {
+                    return Promise.reject(validationState);
+                }
 
-        return Promise.reject('Model status is not valid');
+                return this.modelDefinition
+                    .save(this)
+                    .then(() => this.dirty = false);
+            });
     }
 
     /**
      * @method validate
      *
-     * @returns {{status: boolean, fields: Array}} Object with a status property that represents if the model is valid or not
-     * the fields array will return the names of the fields that are invalid.
+     * @returns {Promise} Promise that resolves with an object with a status property that represents if the model
+     * is valid or not the fields array will return the names of the fields that are invalid.
      *
      * @description
      * This will run the validations on the properties which have validations set. Normally these validations are defined
-     * through the DHIS2 schema. It will check min/max for strings/numbers etc.
+     * through the DHIS2 schema. It will check min/max for strings/numbers etc. Additionally it will
+     * run model validations against the schema.
      *
      * ```js
-     * let modelValidation = myModel.validate();
-     * if (myModel.status === false) {
-     *   myModel.fields.forEach((fieldName) => console.log(fieldName));
-     * }
+     * myModel.validate()
+     *  .then(myModelStatus => {
+     *    if (myModelStatus.status === false) {
+     *      myModelStatus.fields.forEach((fieldName) => console.log(fieldName));
+     *    }
+     * });
      * ```
      */
     validate() {
-        let invalidFields = [];
+        return new Promise((resolve, reject) => {
+            let validationMessages = [];
+            let validationState;
 
-        Object.keys(this.modelDefinition.modelValidations).forEach((propertyName) => {
-            if (!modelValidator.validate(this.dataValues[propertyName], this.modelDefinition.modelValidations[propertyName])) {
-                invalidFields.push(propertyName);
-            }
+            //Run local validation on the models data values
+            validationMessages = validationMessages.concat(
+                localValidation(this.modelDefinition.modelValidations, this.dataValues)
+            );
+
+            //Run async validation against the api
+            asyncRemoteValidation(this)
+                .then(remoteMessages => {
+                    validationMessages = validationMessages.concat(remoteMessages);
+
+                    validationState = {
+                        status: !validationMessages.length,
+                        fields: validationMessages.map(validationMessage => validationMessage.property),
+                        messages: validationMessages
+                    };
+                    resolve(validationState);
+                })
+                .catch(message => reject(message));
         });
 
-        return {
-            status: !invalidFields.length,
-            fields: invalidFields
-        };
+        function localValidation(modelValidations, dataValues) {
+            let validationMessages = [];
+
+            Object.keys(modelValidations).forEach((propertyName) => {
+                let validationStatus = modelValidator.validate(modelValidations[propertyName], dataValues[propertyName]);
+                if (!validationStatus.status) {
+                    validationMessages = validationMessages.concat(validationStatus.messages || []);
+                }
+            });
+
+            return validationMessages;
+        }
+
+        function asyncRemoteValidation(model) {
+            return modelValidator.validateAgainstSchema(model);
+        }
     }
 }
 

test/unit/model/ModelBase_spec.js
@@ -1,10 +1,12 @@
 function setupFakeModelValidation() {
     let validateSpy = sinon.stub();
+    let validateAgainstSchemaSpy = sinon.stub();
     let proxyquire = require('proxyquire').noCallThru();
 
     class ModelValidation {
         constructor() {
             this.validate = validateSpy;
+            this.validateAgainstSchema = validateAgainstSchemaSpy;
         }
     }
     ModelValidation.getModelValidation = function () {
@@ -15,18 +17,19 @@ function setupFakeModelValidation() {
         'd2/model/ModelValidation': ModelValidation
     });
 
-    return validateSpy;
+    return [validateSpy, validateAgainstSchemaSpy];
 }
 
 describe('ModelBase', () => {
     'use strict';
 
     //TODO: For some reason we have to setup the mock before the beforeEach and reset the spy, should figure out a way to perhaps do this differently.
-    let validateSpy = setupFakeModelValidation();
+    let [validateSpy, validateAgainstSchemaSpy] = setupFakeModelValidation();
     let modelBase;
 
     beforeEach(() => {
         validateSpy.reset();
+        validateAgainstSchemaSpy.reset();
 
         modelBase = require('d2/model/ModelBase');
     });
@@ -51,7 +54,7 @@ describe('ModelBase', () => {
             class Model{
                 constructor(modelDefinition) {
                     this.modelDefinition = modelDefinition;
-                    this.validate = stub().returns({status: true});
+                    this.validate = stub().returns(Promise.resolve({status: true}));
                     this.dirty = true;
                 }
             }
@@ -60,10 +63,12 @@ describe('ModelBase', () => {
             model = new Model(modelDefinition);
         });
 
-        it('should call the save on the model modelDefinition with itself as a property', () => {
-            model.save();
-
-            expect(modelDefinition.save).to.have.been.calledWith(model);
+        it('should call the save on the model modelDefinition with itself as a parameter', (done) => {
+            model.save()
+                .then(() => {
+                    expect(modelDefinition.save).to.have.been.calledWith(model);
+                    done();
+                });
         });
 
         it('should call validate before calling save', () => {
@@ -94,7 +99,7 @@ describe('ModelBase', () => {
                 });
         });
 
-        it('should return rejected promise when the model is not dirty', function (done) {
+        it('should return rejected promise when the model is not dirty', (done) => {
             model.dirty = false;
 
             model.save()
@@ -105,12 +110,12 @@ describe('ModelBase', () => {
 
         });
 
-        it('should return rejected promise when the model is not valid', function (done) {
-            model.validate.returns({status: false});
+        it('should return rejected promise when the model is not valid', (done) => {
+            model.validate.returns(Promise.resolve({status: false}));
 
             model.save()
                 .catch(function (message) {
-                    expect(message).to.equal('Model status is not valid');
+                    expect(message).to.deep.equal({status: false});
                     done();
                 });
         });
@@ -150,9 +155,11 @@ describe('ModelBase', () => {
             Model.prototype = modelBase;
             model = new Model(modelValidations);
 
-            validateSpy.returns(true);
-            validateSpy.onFirstCall().returns(true);
-            validateSpy.onSecondCall().returns(true);
+            validateSpy.returns({status: true});
+            validateSpy.onFirstCall().returns({status: true});
+            validateSpy.onSecondCall().returns({status: true});
+
+            validateAgainstSchemaSpy.returns(Promise.resolve([]));
         });
 
         it('should call validate on the model validator for each of the validations', () => {
@@ -160,7 +167,10 @@ describe('ModelBase', () => {
             modelValidations.firstName = {};
             modelValidations.lastName = {};
 
-            model.validate();
+            model.validate()
+                .then(function () {
+
+                });
 
             expect(validateSpy).to.have.callCount(4);
         });
@@ -168,38 +178,107 @@ describe('ModelBase', () => {
         it('should call validate on the ModelValidation with the correct data', () => {
             model.validate();
 
-            expect(validateSpy).to.have.been.calledWith(4, modelValidations.age);
+            expect(validateSpy).to.have.been.calledWith(modelValidations.age, 4);
         });
 
-        it('should return true when the value is correct', () => {
-            expect(model.validate().status).to.be.true;
+        it('should return true when the value is correct', (done) => {
+            model.validate()
+                .then(validationState => {
+                    expect(validationState.status).to.be.true;
+                    done();
+                });
         });
 
-        it('should return false when the validation fails', () => {
-            validateSpy.onFirstCall().returns(false);
+        it('should return false when the validation fails', (done) => {
+            validateSpy.onFirstCall().returns({
+                status: false,
+                messages: [{
+                    message: 'Required property missing',
+                    property: 'name',
+                    value: ''
+                }]
+            });
 
-            expect(model.validate().status).to.be.false;
+            model.validate()
+                .then(validationState => {
+                    expect(validationState.status).to.be.false;
+                    done();
+                });
         });
 
-        it('should return false when one of the validations returns false', () => {
+        it('should return false when one of the validations returns false', (done) => {
+            //Some fake validator that does nothing but triggers a validation call
             modelValidations.name = {};
-            validateSpy.onSecondCall().returns(false);
 
-            expect(model.validate().status).to.be.false;
+            validateSpy.onSecondCall().returns({
+                status: false,
+                messages: [{
+                    message: 'Required property missing',
+                    property: 'name',
+                    value: ''
+                }]
+            });
+
+            model.validate()
+                .then(validationState => {
+                    expect(validationState.status).to.be.false;
+                    done();
+                });
         });
 
-        it('should return true when all validations return true', () => {
+        it('should return true when all validations return true', (done) => {
             modelValidations.name = {};
 
-            expect(model.validate().status).to.be.true;
+            model.validate()
+                .then(validationState => {
+                    expect(validationState.status).to.be.true;
+                    done();
+                });
         });
 
-        it('should return a list of invalid fields', () => {
-            validateSpy.onFirstCall().returns(false);
+        it('should return a list of invalid fields', (done) => {
+            validateSpy.onFirstCall().returns({
+                status: false,
+                messages: [{
+                    message: 'Required property missing',
+                    property: 'age',
+                    value: ''
+                }]
+            });
 
             model.dataValues.age = -1;
 
-            expect(model.validate().fields).to.deep.equal(['age']);
+            model.validate()
+                .then(validationState => {
+                    expect(validationState.fields).to.deep.equal(['age']);
+                    done();
+                });
+        });
+
+        it('should fail when the async validate fails', (done) => {
+            validateAgainstSchemaSpy.returns(Promise.reject('Validation against schema endpoint failed.'));
+
+            model.validate()
+                .catch(message => {
+                    expect(message).to.equal('Validation against schema endpoint failed.');
+                    done();
+                });
+        });
+
+        it('should call the validateAgainstSchema method on the modelValidator', (done) => {
+            model.validate()
+                .then(() => {
+                    expect(validateAgainstSchemaSpy).to.be.called;
+                    done();
+                });
+        });
+
+        it('should call validateAgainstSchema with the model', (done) => {
+            model.validate()
+                .then(() => {
+                    expect(validateAgainstSchemaSpy).to.be.calledWith(model);
+                    done();
+                });
         });
     });
 });

src/model/ModelDefinition.js
@@ -127,6 +127,7 @@ class ModelDefinition {
      *
      * @note {warning} This should generally not be accessed directly.
      */
+    //TODO: check the return status of the save to see if it was actually successful and not ignored
     save(model) {
         let objectToSave = {};
         let ownedProperties = this.getOwnedPropertyNames();

src/model/ModelValidation.js
@@ -4,39 +4,73 @@
 import {checkType, isInteger, isObject, isArray, isString, isNumeric} from 'd2/lib/check';
 import Logger from 'd2/logger/Logger';
 
-var logger;
-var typeSpecificValidations = {
+let logger;
+let typeSpecificValidations = {
     PHONENUMBER: [phoneNumber]
 };
 
+/**
+ * @class ModelBase
+ */
 class ModelValidation {
     constructor(providedLogger) {
         checkType(providedLogger, 'object', 'logger (Logger)');
         logger = providedLogger;
     }
 
-    validate(value, validationSettings) {
-        if (isObject(validationSettings)) {
-
-            //No value when not required is a valid value.
-            if (validationSettings.required === false && !value) {
-                return true;
-            }
-
-            return typeValidation(value, validationSettings.type) &&
-                minMaxValidation(value, validationSettings) &&
-                typeSpecificValidation(value, validationSettings.type);
+    /**
+     * @method validate
+     *
+     * @param {Object} validationSettings
+     * @param {*} value The value to be validated
+     * @returns {{status: boolean, messages: Array}} Returns an object with the status. When the status is false the messages
+     * array will contain messages on why the validation failed.
+     *
+     * @description
+     * Validate a given value against the given validationSettings.
+     * This checks if the value is of the defined `validationSettings.type`
+     * if the value adheres to the set `validationSettings.min` and `validationSettings.max`
+     * and runs any type specific validations like for example on the type PHONENUMBER if it is [0-9+ ] compliant.
+     */
+    validate(validationSettings, value) {
+        if (!isObject(validationSettings)) {
+            throw new TypeError('validationSettings should be of type object');
         }
-        return false;
+        let status = false;
+
+        //No value when not required is a valid value.
+        if (validationSettings.required === false && !value) {
+            return {status: true};
+        }
+
+        status = typeValidation(value, validationSettings.type) &&
+            minMaxValidation(value, validationSettings) &&
+            typeSpecificValidation(value, validationSettings.type);
+
+        return {status: status};
+    }
+
+    validateAgainstSchema() {
+        return Promise.resolve([]);
+    }
+
+    /**
+     * @method getModelValidation
+     * @static
+     *
+     * @returns {ModelValidation}
+     *
+     * @description
+     * Returns the `ModelValidation` singleton. Creates a new one if it does not yet exist.
+     * Grabs a logger instance by calling `Logger.getLogger`
+     */
+    static getModelValidation() {
+        if (this.modelValidation) {
+            return this.modelValidation;
+        }
+        return (this.modelValidation = new ModelValidation(Logger.getLogger(console)));
     }
 }
-
-ModelValidation.getModelValidation = function () {
-    if (this.modelValidation) {
-        return this.modelValidation;
-    }
-    return (this.modelValidation = new ModelValidation(Logger.getLogger(console)));
-};
 
 //TODO: See if we can reduce the complexity of this function
 function typeValidation(value, type) { //jshint maxcomplexity: 16

commit 15749bacd6df5057670b84ba2d116642c9cbd29f

model: Fix false positive that could on validation
Now checks for the status property on the validation status
for the client side validations instead of looking at the
number of messages.

Fixes bug introduced in d16392b0a61226454058869627ad2fae630b583e

src/model/ModelBase.js
@@ -61,6 +61,7 @@ class ModelBase {
      */
     validate() {
         return new Promise((resolve, reject) => {
+            let modelValidationStatus = true;
             let validationMessages = [];
             let validationState;
 
@@ -75,31 +76,30 @@ class ModelBase {
                     validationMessages = validationMessages.concat(remoteMessages);
 
                     validationState = {
-                        status: !validationMessages.length,
+                        status: modelValidationStatus,
                         fields: validationMessages.map(validationMessage => validationMessage.property),
                         messages: validationMessages
                     };
                     resolve(validationState);
                 })
                 .catch(message => reject(message));
-        });
 
-        function localValidation(modelValidations, dataValues) {
-            let validationMessages = [];
+            function localValidation(modelValidations, dataValues) {
+                let validationMessages = [];
 
-            Object.keys(modelValidations).forEach((propertyName) => {
-                let validationStatus = modelValidator.validate(modelValidations[propertyName], dataValues[propertyName]);
-                if (!validationStatus.status) {
+                Object.keys(modelValidations).forEach((propertyName) => {
+                    let validationStatus = modelValidator.validate(modelValidations[propertyName], dataValues[propertyName]);
+                    modelValidationStatus = modelValidationStatus && validationStatus.status;
                     validationMessages = validationMessages.concat(validationStatus.messages || []);
-                }
-            });
+                });
 
-            return validationMessages;
-        }
+                return validationMessages;
+            }
 
-        function asyncRemoteValidation(model) {
-            return modelValidator.validateAgainstSchema(model);
-        }
+            function asyncRemoteValidation(model) {
+                return modelValidator.validateAgainstSchema(model);
+            }
+        });
     }
 }
 

test/unit/model/ModelBase_spec.js
@@ -280,5 +280,18 @@ describe('ModelBase', () => {
                     done();
                 });
         });
+
+        it('should not return a false positive when there are no messages', (done) => {
+            validateSpy.onFirstCall().returns({
+                status: false,
+                messages: []
+            });
+
+            model.validate()
+                .then(validationState => {
+                    expect(validationState.status).to.be.false;
+                    done();
+                });
+        });
     });
 });