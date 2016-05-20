c6f53c3512d322353092f09cb2c40e5ddef919f0
Mark Polak <mark@thedutchies.com>
2016-05-20 11:25:01 +0200

    adds model defaults for dataElement
    aggregationType is by default set to NONE
    valueType is by default set to TEXT
    
    References dhis2/maintenance-app#157

--- src/model/config/index.js
+++ src/model/config/index.js
@@ -1,6 +1,7 @@
 import organisationUnitGroupSetDefaults from './model-defaults/organisationUnitGroupSet';
 import categoryDefaults from './model-defaults/category';
 import categoryOptionGroupSetDefaults from './model-defaults/categoryOptionGroupSet';
+import dataElementDefaults from './model-defaults/dataElement';
 import dataElementGroupSetDefaults from './model-defaults/dataElementGroupSet';
 import dataSetDefaults from './model-defaults/dataSet';
 
@@ -8,6 +9,7 @@ export const defaultValues = new Map([
     ['organisationUnitGroupSet', organisationUnitGroupSetDefaults],
     ['category', categoryDefaults],
     ['categoryOptionGroupSet', categoryOptionGroupSetDefaults],
+    ['dataElement', dataElementDefaults],
     ['dataElementGroupSet', dataElementGroupSetDefaults],
     ['dataSet', dataSetDefaults],
 ]);
---
+++ src/model/config/model-defaults/dataElement.js
@@ -0,0 +1,4 @@
+export default {
+    aggregationType: 'NONE',
+    valueType: 'TEXT',
+};
