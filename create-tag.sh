14b891bf40a8cd628f8e67bf6a56d4393ca5f2b9
Mark Polak <mark@thedutchies.com>
2016-08-02 10:46:08 +0200

    Don't show output in log

--- create-tag.sh
+++ create-tag.sh
@@ -27,8 +27,8 @@ if [[ $isCreateTag == true ]]; then
     git config user.name "Travis CI"
     git config user.email "mark@dhis2.org"
     npm version patch && npm run build
-    git push https://${GITHUB_TOKEN}@${GH_REF} HEAD:$TRAVIS_BRANCH --quiet
-    git push https://${GITHUB_TOKEN}@${GH_REF} --tags --quiet
+    git push --quiet https://${GITHUB_TOKEN}@${GH_REF} HEAD:$TRAVIS_BRANCH > /dev/null 2>&1
+    git push --quiet https://${GITHUB_TOKEN}@${GH_REF} --tags > /dev/null 2>&1
 else
     echo "No tag";
 fi
