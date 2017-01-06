68e9b21cee21fb65e22a4aa89214b5ed4d34597c
Nicolay Ramm <nicolay@dhis2.org>
2017-01-06 14:40:13 +0100

    Remove debug logging

--- src/model/ModelValidation.js
+++ src/model/ModelValidation.js
@@ -50,10 +50,7 @@ class ModelValidation {
 
         // TODO: The function getOwnedPropertyJSON should probably not be exposed, perhaps we could have a getJSONForModel(ownedPropertiesOnly=true) method.
         return Api.getApi().post(url, getOwnedPropertyJSON(model))
-            .catch((e) => {
-                console.error(e);
-                return Promise.reject(e);
-            })
+            .catch(e => Promise.reject(e))
             .then((webMessage) => {
                 if (webMessage.status === 'OK') {
                     return [];
