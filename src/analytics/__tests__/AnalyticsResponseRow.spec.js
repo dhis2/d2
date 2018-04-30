0f597f8b3344cda5e2c8495905c0d3ec77120da1
Edoardo Sabadelli <edoardo@dhis2.org>
2018-04-30 11:31:47 +0200

    Add initial tests for AnalyticsResponse classes

---
+++ src/analytics/__tests__/AnalyticsResponseRow.spec.js
@@ -0,0 +1,44 @@
+import AnalyticsResponseRow from '../AnalyticsResponseRow';
+
+let responseRow;
+const row = ['Male', '201706', 'O6uvpzGd5pu', '47'];
+
+describe('AnalyticsResponseRow', () => {
+    beforeEach(() => {
+        responseRow = new AnalyticsResponseRow(row);
+    });
+
+    describe('constructor', () => {
+        it('should not be allowed to be called without new', () => {
+            expect(() => AnalyticsResponseRow()).toThrowError('Cannot call a class as a function'); // eslint-disable-line new-cap
+        });
+
+        it('should set the row when passed as argument', () => {
+            expect(responseRow).toEqual(row);
+        });
+    });
+
+    describe('properties', () => {
+        describe('.getAt()', () => {
+            it('should return the value at the given index', () => {
+                expect(responseRow.getAt(1)).toEqual('201706');
+            });
+        });
+
+        describe('.setIdCombination()', () => {
+            it('should set the given id combination', () => {
+                responseRow.setIdCombination('test-id-combination');
+
+                expect(responseRow.idCombination).toEqual('test-id-combination');
+            });
+        });
+
+        describe('.toFloat()', () => {
+            it('should return the value parsed as floating point number', () => {
+                responseRow.toFloat(1);
+
+                expect(responseRow[1]).toEqual(201706);
+            });
+        });
+    });
+});
