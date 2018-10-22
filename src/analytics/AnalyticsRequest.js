40cf6277f788a1cc18d79cad9fc24b82ef6348ec
Edoardo Sabadelli <edoardo@dhis2.org>
2018-10-22 15:59:56 +0200

    Implement optional flag for passing filters as dimensions
    Required when building analytics dataValueSet requests.

--- src/analytics/AnalyticsRequest.js
+++ src/analytics/AnalyticsRequest.js
@@ -17,12 +17,13 @@ import AnalyticsRequestBase from './AnalyticsRequestBase';
  * @extends module:analytics.AnalyticsRequestBase
  */
 class AnalyticsRequest extends AnalyticsRequestDimensionsMixin(
-    AnalyticsRequestFiltersMixin(AnalyticsRequestPropertiesMixin(AnalyticsRequestBase)),
+    AnalyticsRequestFiltersMixin(AnalyticsRequestPropertiesMixin(AnalyticsRequestBase))
 ) {
     /**
      * Extracts dimensions and filters from an analytic object model and add them to the request
      *
      * @param {Object} model The analytics object model from which extract the dimensions/filters
+     * @param {Boolean} [passFilterAsDimension=false] Pass filters as dimension in the query string (used in dataValueSet requests)
      *
      * @returns {AnalyticsRequest} A new instance of the class for chaining purposes
      *
@@ -31,15 +32,20 @@ class AnalyticsRequest extends AnalyticsRequestDimensionsMixin(
      *    .fromModel(model);
      *
      * // dimension=pe:LAST_12_MONTH&dimension=dx:fbfJHSPpUQD;cYeuwXTCPkU;Jtf34kNZhzP;hfdmMSPBgLG&filter=ou:ImspTQPwCqd
+     *
+     * const req2 = new d2.analytics.request()
+     *    .fromModel(model, true);
+     *
+     * // dimension=pe:LAST_12_MONTH&dimension=dx:fbfJHSPpUQD;cYeuwXTCPkU;Jtf34kNZhzP;hfdmMSPBgLG&dimension=ou:ImspTQPwCqd
      */
-    fromModel(model) {
+    fromModel(model, passFilterAsDimension = false) {
         let request = this;
 
         // extract dimensions from model
         const columns = model.columns || [];
         const rows = model.rows || [];
 
-        columns.concat(rows).forEach((d) => {
+        columns.concat(rows).forEach(d => {
             let dimension = d.dimension;
 
             if (d.filter) {
@@ -52,9 +58,11 @@ class AnalyticsRequest extends AnalyticsRequestDimensionsMixin(
         // extract filters from model
         const filters = model.filters || [];
 
-        filters.forEach(
-            f => (request = request.addFilter(f.dimension, f.items.map(item => item.id))),
-        );
+        filters.forEach(f => {
+            request = passFilterAsDimension
+                ? request.addDimension(f.dimension, f.items.map(item => item.id))
+                : request.addFilter(f.dimension, f.items.map(item => item.id));
+        });
 
         return request;
     }
