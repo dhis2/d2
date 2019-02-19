8a13c0f0f00f392cac7736adfc9c77447ae8186f
ismay <ismay@ismaywolff.nl>
2019-02-19 10:49:28 +0100

    Update ModelBase test

--- src/model/__tests__/ModelBase.spec.js
+++ src/model/__tests__/ModelBase.spec.js
@@ -3,7 +3,7 @@ import ModelBase, { DIRTY_PROPERTY_LIST } from '../ModelBase';
 
 jest.mock('../ModelValidation');
 
-describe.skip('ModelBase', () => {
+describe('ModelBase', () => {
     // TODO: For some reason we have to setup the mock before the beforeEach and reset the spy, should figure out a way to perhaps do this differently.
     let validateAgainstSchemaSpy;
 
